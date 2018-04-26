819c0a0f4094636b6aeca2cb664631c587498666
gehrig rankin <rgrankin22@gmail.com>
2018-04-25 17:12:11 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3500;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+});
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+});
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
