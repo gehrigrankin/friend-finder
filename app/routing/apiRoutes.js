8196a57ba68eecfa9339fb45c1e4a336b83b0232
gehrig rankin <rgrankin22@gmail.com>
2018-04-26 23:55:02 -0700

    final commit except modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,13 +15,44 @@ module.exports = function(app){
     
         console.log(req.body);
 
-        var score1 = newFriend.scores
+        var score1 = newFriend.scores[0];
+        var score2 = newFriend.scores[1];
+        var score3 = newFriend.scores[2];
+        var score4 = newFriend.scores[3];
+
+        var bestfriend = friends[0];
+
+        
+
 
         for (var i = 0; i < friends.length; i++){
-            var fScore1 = friends[i].scores;
-            console.log(fScore1);
+
+            var bestScoreDiff = [
+                Math.abs(score1 - bestfriend.scores[0]),
+                Math.abs(score2 - bestfriend.scores[1]),
+                Math.abs(score3 - bestfriend.scores[2]),
+                Math.abs(score4 - bestfriend.scores[3])  
+            ];
+
+            var scoreDiff = [
+                Math.abs(score1 - friends[i].scores[0]),
+                Math.abs(score2 - friends[i].scores[1]),
+                Math.abs(score3 - friends[i].scores[2]),
+                Math.abs(score4 - friends[i].scores[3])
+            ];
+
+
+            var bestfriendScore = bestScoreDiff.reduce((a, b) => a + b, 0);
+
+            var currentScore = scoreDiff.reduce((a, b) => a + b, 0);
+
+            if(bestfriendScore > currentScore){
+                bestfriend = friends[i];
+            }
+
         }
 
+        console.log('your bff is: ' + bestfriend.name);
 
     
         friends.push(newFriend);
